32bc0d05eb20ec46d4495508973dfeb6716376cc
Bruno <bruno.silva.castilho@gmail.com>
2023-12-24 12:58:07 -0300

    Saving state in storage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -21,7 +21,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const diffSeconds = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
         if (diffSeconds >= totalSeconds) {
           markCurrentCycleAsFinished()
